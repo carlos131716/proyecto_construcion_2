a0c8463d363442428d441089bae17a78ab0ea74d
MakeinGon <makein.gonzales@unas.edu.pe>
2026-06-15 17:05:36 -0500

    Base inicial del proyecto DIAGTI

---
+++ backend/src/main/java/pe/edu/unas/ctic/diagti/DiagtiApiApplication.java
@@ -0,0 +1,16 @@
+package pe.edu.unas.ctic.diagti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagtiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagtiApiApplication.class, args);
+	}
+
+}
+
+
+
---
+++ backend/src/test/java/pe/edu/unas/ctic/diagti/DiagtiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unas.ctic.diagti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DiagtiApiApplication.class)
+class DiagtiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
